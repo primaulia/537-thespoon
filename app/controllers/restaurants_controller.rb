dd3543fa738bc20df6859d5fbe32bdc7a8852afd
Prima Aulia Gusta <primaulia@gmail.com>
2021-02-17 12:05:14 +0800

    livecode

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,55 @@
+class RestaurantsController < ApplicationController
+  # filter
+  # before_action :<method_name>, except: [:<action1>]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @new_restaurant = Restaurant.new
+  end
+
+  def create
+    @new_restaurant = Restaurant.new(strong_params)
+
+    if @new_restaurant.save
+      redirect_to show_restaurant_path(@new_restaurant)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(strong_params)
+      redirect_to show_restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @restaurant.destroy
+      redirect_to restaurants_path
+    end
+  end
+
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:restaurant).permit(:name, :rating, :address, :chef_name)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_013720) do
+ActiveRecord::Schema.define(version: 2021_02_17_015007) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_013720) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "chef_name"
   end
 
 end
